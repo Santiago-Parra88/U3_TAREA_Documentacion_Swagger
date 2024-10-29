a14ddbc9cffba7d9048083bcebb9da82f968a2f0
Santiago-Parra88 <sparrar@est.ups.edu.ec>
2024-10-28 23:14:14 -0500

    Coreccion Error por espacios
    Se genera los metodos faltantes.

--- api_rest/network/routes.js
+++ api_rest/network/routes.js
@@ -6,8 +6,8 @@ const usuario = require('../components/usuario/interface')
  *     name: usuario
  *     description: Operaciones relacionadas con usuarios
  * 
- * /
-
+ */
+//Se escpecifica la ruta Agregar Usuario
 /**
  * @swagger
  * /usuario:
@@ -15,7 +15,7 @@ const usuario = require('../components/usuario/interface')
  *     tags:
  *       - Registra Usuario
  *     summary: Inserta un nuevo usuario
- *     description: Crea un nuevo usuario con nombre y apellido. Devuelve el objeto del usuario creado con la fecha de registro.    
+ *     description: Crea un nuevo usuario recibiendo como parametro nombre y apellido. Devuelve el objeto del usuario creado con la fecha de registro.    
  *     requestBody:
  *       required: true
  *       content:
@@ -54,24 +54,26 @@ const usuario = require('../components/usuario/interface')
  *                   type: string
  *                   format: date-time
  *                   description: Fecha de Creacion
- *                   example: 2024-10-27T23:17:222
+ *                   example: 2024-10-27T23:17:22Z
  *                 fecha_actualizacion:
  *                   type: string
  *                   format: date-time
- *                   description: Fecha de Creacion
- *                   example: 2024-10-27T23:17:222
+ *                   description: Fecha de Actualizacion
+ *                   example: 2024-10-27T23:17:22Z
  *       400:
  *         description: Error en la creación del usuario
  */
-
+//Se escpecifica la ruta Listar Usuarios
 /**
  * @swagger
  * /usuario:
  *   get:
- *     summary: Obtiene un usuario
+ *     tags:
+ *       - Obtiene Usuarios
+ *     summary: Obtiene un usuarios
  *     responses:
  *       200:
- *         description: Usuario obtenido con éxito
+ *         description: Usuario recuperado con éxito
  *         content:
  *           application/json:
  *             schema:
@@ -98,6 +100,110 @@ const usuario = require('../components/usuario/interface')
  *                         format: date-time
  *       400:
  *         description: Error al obtener el usuario
+ */
+//Se escpecifica la ruta actualizar Usuario
+/**
+ * @swagger
+ * /usuario:
+ *   put:
+ *     tags:
+ *       - Actualiza Usuario
+ *     summary: Actualizar un usuario
+ *     description: Metodo para Actualiza la información de un usuario existente en la base de datos segun el id.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *                 description: ID único del usuario a actualizar.
+ *                 example: "6709dd71edfd42118d956628"
+ *               nombre:
+ *                 type: string
+ *                 description: Nombre del usuario.
+ *                 example: "Santiago"
+ *               apellido:
+ *                 type: string
+ *                 description: Apellido del usuario.
+ *                 example: "Parra"
+ *     responses:
+ *       200:
+ *         description: Usuario actualizado exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   description: Información actualizada del usuario.
+ *       400:
+ *         description: Error al actualizar el usuario.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: Mensaje de error.
+ */
+//Se escpecifica la ruta eliminar Usuario
+/**
+ * @swagger
+ * /usuario:
+ *   delete:
+ *     tags:
+ *       - Eliminar Usuario
+ *     summary: Eliminar un usuario
+ *     description: Elimina un usuario existente en la base de datos mediante parametro ID.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *                 description: ID único del usuario a eliminar.
+ *                 example: "6709dd61edfd42118d956625"
+ *     responses:
+ *       200:
+ *         description: Usuario eliminado exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   description: Información del usuario eliminado.
+ *       400:
+ *         description: Error al eliminar el usuario.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 error:
+ *                   type: string
+ *                   description: Mensaje de error.
  */
 const routes = function( server ) {
     server.use('/usuario', usuario)
